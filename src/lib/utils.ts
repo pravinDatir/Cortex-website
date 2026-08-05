1d62e63439a4a4a501b93a26ab3ff93dc496f280
Suraj Santosh Pawar <surajpawar913@gmail.com>
2026-08-05 22:32:24 +0530

    done

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,8 +3,6 @@ export function cn(...classes: (string | undefined | false | null)[]): string {
 }
 
 export function getAssetPath(path: string): string {
-  const isGitHubPages = typeof process !== "undefined" && process.env.GITHUB_ACTIONS === "true";
-  const basePath = isGitHubPages ? "/Cortex-website" : "";
   const cleanPath = path.startsWith("/") ? path : `/${path}`;
-  return `${basePath}${cleanPath}`;
+  return cleanPath;
 }
